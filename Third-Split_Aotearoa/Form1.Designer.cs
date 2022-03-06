e279654f90e72ea93be9ada63bc20397206ac807
Thom-Geb <t.gebel@zebresel.com>
2022-03-06 16:42:15 +0100

    Projektdateien hinzufügen.

---
+++ Third-Split_Aotearoa/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Third_Split_Aotearoa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.button_folderDialog = new System.Windows.Forms.Button();
+            this.label_chosenFolder = new System.Windows.Forms.Label();
+            this.listBox_folderContents = new System.Windows.Forms.ListBox();
+            this.label_folderContents = new System.Windows.Forms.Label();
+            this.button_splitMP3 = new System.Windows.Forms.Button();
+            this.button_addFiles = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label_helptext = new System.Windows.Forms.Label();
+            this.textBox_splitSize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_folderDialog
+            // 
+            this.button_folderDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_folderDialog.Location = new System.Drawing.Point(16, 71);
+            this.button_folderDialog.Name = "button_folderDialog";
+            this.button_folderDialog.Size = new System.Drawing.Size(95, 40);
+            this.button_folderDialog.TabIndex = 0;
+            this.button_folderDialog.Text = "Add Folder";
+            this.button_folderDialog.UseVisualStyleBackColor = true;
+            this.button_folderDialog.Click += new System.EventHandler(this.button_folderDialog_Click);
+            // 
+            // label_chosenFolder
+            // 
+            this.label_chosenFolder.AutoSize = true;
+            this.label_chosenFolder.Location = new System.Drawing.Point(113, 71);
+            this.label_chosenFolder.Name = "label_chosenFolder";
+            this.label_chosenFolder.Size = new System.Drawing.Size(16, 13);
+            this.label_chosenFolder.TabIndex = 1;
+            this.label_chosenFolder.Text = "...";
+            // 
+            // listBox_folderContents
+            // 
+            this.listBox_folderContents.FormattingEnabled = true;
+            this.listBox_folderContents.Location = new System.Drawing.Point(12, 142);
+            this.listBox_folderContents.Name = "listBox_folderContents";
+            this.listBox_folderContents.Size = new System.Drawing.Size(636, 186);
+            this.listBox_folderContents.TabIndex = 2;
+            // 
+            // label_folderContents
+            // 
+            this.label_folderContents.AutoSize = true;
+            this.label_folderContents.Location = new System.Drawing.Point(13, 123);
+            this.label_folderContents.Name = "label_folderContents";
+            this.label_folderContents.Size = new System.Drawing.Size(114, 13);
+            this.label_folderContents.TabIndex = 3;
+            this.label_folderContents.Text = "For Split Selected Files";
+            // 
+            // button_splitMP3
+            // 
+            this.button_splitMP3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_splitMP3.Location = new System.Drawing.Point(539, 344);
+            this.button_splitMP3.Name = "button_splitMP3";
+            this.button_splitMP3.Size = new System.Drawing.Size(109, 32);
+            this.button_splitMP3.TabIndex = 4;
+            this.button_splitMP3.Text = "SPLIT";
+            this.button_splitMP3.UseVisualStyleBackColor = true;
+            this.button_splitMP3.Click += new System.EventHandler(this.button_splitMP3_Click);
+            // 
+            // button_addFiles
+            // 
+            this.button_addFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_addFiles.Location = new System.Drawing.Point(16, 12);
+            this.button_addFiles.Name = "button_addFiles";
+            this.button_addFiles.Size = new System.Drawing.Size(95, 41);
+            this.button_addFiles.TabIndex = 5;
+            this.button_addFiles.Text = "Add Files";
+            this.button_addFiles.UseVisualStyleBackColor = true;
+            this.button_addFiles.Click += new System.EventHandler(this.button_addFiles_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = ".mp3";
+            this.openFileDialog.Filter = "mp3 files(*.mp3)|*.mp3|All files(*.*)|*.*";
+            // 
+            // label_helptext
+            // 
+            this.label_helptext.AutoSize = true;
+            this.label_helptext.Location = new System.Drawing.Point(327, 379);
+            this.label_helptext.Name = "label_helptext";
+            this.label_helptext.Size = new System.Drawing.Size(321, 26);
+            this.label_helptext.TabIndex = 6;
+            this.label_helptext.Text = "The program automatically splits your files by the given time/frame. \r\nA new dire" +
+    "ctory is added with the same name as the file.";
+            // 
+            // textBox_splitSize
+            // 
+            this.textBox_splitSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_splitSize.Location = new System.Drawing.Point(432, 347);
+            this.textBox_splitSize.Name = "textBox_splitSize";
+            this.textBox_splitSize.Size = new System.Drawing.Size(101, 26);
+            this.textBox_splitSize.TabIndex = 7;
+            this.textBox_splitSize.Text = "300";
+            this.textBox_splitSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(428, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Split every (seconds)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 414);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_splitSize);
+            this.Controls.Add(this.label_helptext);
+            this.Controls.Add(this.button_addFiles);
+            this.Controls.Add(this.button_splitMP3);
+            this.Controls.Add(this.label_folderContents);
+            this.Controls.Add(this.listBox_folderContents);
+            this.Controls.Add(this.label_chosenFolder);
+            this.Controls.Add(this.button_folderDialog);
+            this.Name = "Form1";
+            this.Text = "Third-Split  -   Split your MP3-Files -   ThG Illusions 2022";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button button_folderDialog;
+        private System.Windows.Forms.Label label_chosenFolder;
+        private System.Windows.Forms.ListBox listBox_folderContents;
+        private System.Windows.Forms.Label label_folderContents;
+        private System.Windows.Forms.Button button_splitMP3;
+        private System.Windows.Forms.Button button_addFiles;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label_helptext;
+        private System.Windows.Forms.TextBox textBox_splitSize;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Third-Split_Aotearoa/Form1.cs
@@ -0,0 +1,137 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Third_Split_Aotearoa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_splitMP3.Enabled = false;
+        }
+
+        private void split_file(string pathToFile) 
+        {
+            string strMP3SourceFilename = pathToFile;              
+            string strMP3OutputFilename = getFileName(pathToFile);
+
+            //use filename as foldername without the extension mp3
+            string strMP3OutputFolder = $"{strMP3SourceFilename.Substring(0, strMP3SourceFilename.Length - 4)}\\";
+
+            //create Directory if it does not exist -> base is the given filename
+            if (!Directory.Exists(strMP3OutputFolder))
+            {
+                Directory.CreateDirectory(strMP3OutputFolder);
+            }
+
+            using (Mp3FileReader reader = new Mp3FileReader(strMP3SourceFilename))
+            {
+                int count = 1;
+                int fragment = 1;
+                Mp3Frame mp3Frame = reader.ReadNextFrame();
+                System.IO.FileStream _fs = new System.IO.FileStream(strMP3OutputFolder + "0" + fragment + " " + strMP3OutputFilename, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+
+                //double audioLengthInSeconds = (double)mp3Frame.SampleCount / mp3Frame.SampleRate; //how long is a frame -> to use for user to set time instead of frame
+                //Console.WriteLine(audioLengthInSeconds.ToString());
+                int frameIndex = convertSecondsToFrame(mp3Frame.SampleCount, mp3Frame.SampleRate);
+
+                while (mp3Frame != null)
+                {
+                    if (count % frameIndex == 0) //retrieve a sample of frames ToDo: -> base on the frame on the kbit/s to let user define by time instead of frame
+                    {
+                        fragment++;
+                        _fs.Close();
+                        _fs = new System.IO.FileStream(strMP3OutputFolder + "0" + fragment + " " + strMP3OutputFilename, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                        
+                    }
+                    _fs.Write(mp3Frame.RawData, 0, mp3Frame.RawData.Length);
+
+                    count++;
+                    mp3Frame = reader.ReadNextFrame();
+                }
+
+                _fs.Close();
+            }
+        }
+
+        private void button_folderDialog_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                label_chosenFolder.Text = folderBrowserDialog.SelectedPath;
+                showMP3FileOnList();
+            }
+        }
+
+        private void button_addFiles_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                listBox_folderContents.Items.Add(openFileDialog.FileName);
+                button_splitMP3.Enabled = true;
+            }
+        }
+
+        private void showMP3FileOnList()
+        {
+            string[] files = { };
+            files = Directory.GetFiles(label_chosenFolder.Text);
+
+            foreach( string file in files )
+            {
+                if (file.Contains(".mp3"))
+                {
+                    listBox_folderContents.Items.Add(file);
+                }
+            }
+            button_splitMP3.Enabled = true;
+        }
+
+        private void button_splitMP3_Click(object sender, EventArgs e)
+        {
+            foreach(string mp3 in listBox_folderContents.Items)
+            {
+                split_file(mp3);
+            }
+        }
+
+        private string getFileName(string pathToFile)
+        {
+            int lastSlash = pathToFile.LastIndexOf('\\');
+            
+            return pathToFile.Substring(lastSlash+1, pathToFile.Length-lastSlash-1);
+        }
+
+        private int convertSecondsToFrame(int sampleCount, int sampleRate)
+        {
+            int givenSeconds = Int32.Parse(textBox_splitSize.Text);
+
+            double frameLengthInSeconds = (double)sampleCount / (double)sampleRate;
+            double framesConverted = Convert.ToDouble(givenSeconds) / frameLengthInSeconds;
+
+            int Value = Convert.ToInt32(framesConverted);
+
+            return Value;
+        }
+
+      
+    }
+}
